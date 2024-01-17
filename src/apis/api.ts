3e6cab4d3945e5f9f3983d2346d4f78fe2647521
Dzmitry Kasenka <dzmitry.kasenka@idt.net>
2024-01-17 16:09:51 +0300

    [WCS-2081] sdk should pass specific header to gateway that can be used to identify sdk language and version

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -6,6 +6,7 @@ export class ZenditApi extends baseAPI {
     constructor(apiKey?: string) {
         const config = new Configuration({
             apiKey: `Bearer ${apiKey}`,
+            headers: {"User-Agent": "ZenditSDK/1.3.1 NodeJS"},
         })
 
         super(config)
