21ba5121e2d1fa536b7e6756e4501d9d5d4115f4
Dzmitry Kasenka <dzmitry.kasenka@idt.net>
2024-02-15 15:59:49 +0300

    [WCS-2394] bump SDK version user-agent header

--- src/apis/api.ts
+++ src/apis/api.ts
@@ -6,7 +6,7 @@ export class ZenditApi extends baseAPI {
     constructor(apiKey?: string) {
         const config = new Configuration({
             apiKey: `Bearer ${apiKey}`,
-            headers: {"User-Agent": "ZenditSDK/1.3.1 NodeJS"},
+            headers: {"User-Agent": "ZenditSDK/1.4.0 NodeJS"},
         })
 
         super(config)
